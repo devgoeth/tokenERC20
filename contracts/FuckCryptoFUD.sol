b3256a03ad6c10af6b24b5699b3504b6d1e47961
Kondratiev Sergey <kondratiev@red-flag.ru>
2021-05-25 06:38:25 +0300

    first commit

---
+++ contracts/FuckCryptoFUD.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+* BEP20 Token.
+* Name: Fuck crypto FUD
+* Symbol: FCFUD
+* Total supply: 1 000 000 FCFUD
+* Decimals: 18
+*
+* https://eips.ethereum.org/EIPS/eip-20
+*/
+contract FuckCryptoFUD is ERC20 {
+
+    /**
+    * @notice Mint 1 000 000 FCFUD tokens
+    */
+    constructor() ERC20("Fuck crypto FUD", "FCFUD") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    /**
+    * @notice Function for burn your tokens
+    *
+    * @param _amount Amount tokens for burn
+    */
+    function burnMyToken(uint _amount) public {
+        _burn(msg.sender, _amount);
+    }
+}
