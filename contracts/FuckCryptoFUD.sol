a7d8da507deedf64b5afb6f8837c6a7cd1d7a268
Kondratiev Sergey <kondratiev@red-flag.ru>
2021-06-26 13:28:03 +0300

    CMC

--- contracts/FuckCryptoFUD.sol
+++ contracts/FuckCryptoFUD.sol
@@ -5,28 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
 * BEP20 Token.
-* Name: Fuck crypto FUD
-* Symbol: FCFUD
-* Total supply: 1 000 000 FCFUD
+* Name: CRYPTO MASTER COIN
+* Symbol: CMC
+* Total supply: 100 000 CMC
 * Decimals: 18
 *
 * https://eips.ethereum.org/EIPS/eip-20
 */
-contract FuckCryptoFUD is ERC20 {
+contract CryptoMasterCoin is ERC20 {
 
     /**
-    * @notice Mint 1 000 000 FCFUD tokens and send to owner
+    * @notice Mint 100 000 CMC tokens and send to owner
     */
-    constructor() ERC20("Fuck Crypto Fud", "FUCKCRYPTOFUD") {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
-    }
-
-    /**
-    * @notice Function for burn your tokens
-    *
-    * @param _amount Amount tokens for burn
-    */
-    function burnMyTokens(uint _amount) public {
-        _burn(msg.sender, _amount);
+    constructor() ERC20("CRYPTO MASTER COIN", "CMC") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
     }
 }
